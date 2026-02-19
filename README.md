e97fe9f047bbc07c912b6baf9ce49a472eeece31
GraphTree01 <antonio01@ciencias.unam.mx>
2026-02-19 15:51:49 -0600

    Práctica 1 lista!

--- README.md
+++ README.md
@@ -22,12 +22,12 @@ Si no les llegan a funcionar, es posible que el problema es que tengan una versi
 
 En esta sección deben eliminar esta línea de texto, borrar la leyenda "Integrante n" y escribir su nombre empezando por apellidos y su número de cuenta.
 
-+ Integrante 1
-    - No. de Cuenta: 
-+ Integrante 2
-    - No. de Cuenta: 
-+ Integrante 3
-    - No. de Cuenta: 
++ Martínez Méndez Ángel Antonio
+    - No. de Cuenta: 322143353 
++ Pérez Pérez Iker Daniel
+    - No. de Cuenta: 322167450
++ Rendón Ávila Jesús Mateo
+    - No. de Cuenta: 309144979
 
 ## Comentarios
 
--- src/Practica01.hs
+++ src/Practica01.hs
@@ -4,66 +4,104 @@ module Practica01 where
 
 --Ejercicio 1
 data Shape = Circle Float | --representa el radio
-            Square Float | --representa un lado
-            Rectangle Float Float| --representa base y altura
-            Triangle Float | --representa un lado
-            Trapeze Float Float Float --representa base mayor, base menor y altura
-            deriving (Show)
+             Square Float | --representa un lado
+             Rectangle Float Float| --representa base y altura
+             Triangle Float | --representa un lado
+             Trapeze Float Float Float --representa base mayor, base menor y altura
+  deriving (Show, Eq)
 
 --Funcion que calcula el area de las figuras
 area :: Shape -> Float
-area = undefined
+area (Circle x) = x * x * 3.14159
+area (Square x) = x * x
+area (Rectangle x y) = x * y
+area (Triangle x) = ((sqrt 3) / 4) * x * x
+area (Trapeze x y z) = (x + y) * z / 2
+
 
 --Funcion que calcula el perimetro de las figuras
 perimeter :: Shape -> Float
-perimeter = undefined
-
+perimeter (Circle x) = 2 * 3.14159 * x
+perimeter (Square x) = 4 * x
+perimeter (Rectangle x y) = (x + y) * 2
+perimeter (Triangle x) = 3 * x
+perimeter (Trapeze x y z) = x + y + 2 * lado
+  where
+    lado = sqrt (z * z + h * h)
+    h = (x - y) / 2
 
 --Ejercicio 2 (Les toca arreglar el sinonimo)
-type Point = Shape
+type Point = (Float, Float) 
 
 -- Funcion para calcular la distancia entre dos puntos
 distance :: Point -> Point -> Float
-distance = undefined 
+distance (a, b) (c, d) = sqrt (dx * dx + dy * dy)
+  where
+    dx = c - a
+    dy = d - b
 
 --Funcion para calcular la distancia de un punto al origen
 from0 :: Point -> Float
-from0 = undefined
+from0 (x, y) = sqrt (x * x + y * y)
 
 --Ejercicio 3
-data Haskellium = Undefined
+data Haskellium = Haskellium {name :: String,
+                               lastName1 :: String,
+                               lastName2 :: String,
+                               location :: Point,
+                               houseShape :: Shape} deriving Show
 
 --Funcion para regresar el hijo de dos Haskelliums dado su nombre
 son :: Haskellium -> Haskellium -> String -> Haskellium
-son = undefined
+son p1 p2 n = Haskellium {name = n,
+  lastName1 = lastName1 p1,
+  lastName2 = lastName1 p2,
+  location = location p1,
+  houseShape = houseShape p1}
 
 --Funcion para calcular las unidades para construir la casa de un Haskellium
 houseCost :: Haskellium -> Float
-houseCost = undefined
+houseCost p = perimeter (houseShape p) * altura + area (houseShape p)
+  where altura = 2.5
 
 --Funcion para calcular el tiempo que le toma a un Haskellium para llegar a su trabajo
 timeToWork :: Haskellium -> Float
-timeToWork = undefined
+timeToWork p
+  | distancia > 299 =  distancia / 70
+  | otherwise = distancia / 30
+  where
+    distancia = from0 (location p)
 
 --LISTAS Y FUNCIONES
 --Ejercicio 1
 palindromo :: String -> Bool
-palindromo = undefined
+palindromo xs = xs == reversa xs
+
+--Función Auxiliar para el Ejercicio 1. Regresa la inversa de una cadena
+reversa :: String -> String
+reversa [] = []
+reversa (x:xs) = reversa xs ++ [x]
 
 --Ejercicio 2
 myFoldr :: (a -> b -> b) -> b -> [a] -> b
-myFoldr = undefined
-
+myFoldr _ x [] = x
+myFoldr f v (x:xs) = f x (myFoldr f v xs)
+                      
 --Ejercicio 3
 conjuntoPotencia :: [a] -> [[a]]
-conjuntoPotencia = undefined
-
+conjuntoPotencia [] = [[]]
+conjuntoPotencia (x:xs) = conjuntoPotencia xs ++ [x:ys | ys <- conjuntoPotencia xs]
 --ARBOLES
 
 --Implementacion
 
-data OneTwoTree a = Undefinedd
+data OneTwoTree a = Void |
+                    Node a (OneTwoTree a) |
+                    Branch a (OneTwoTree a) (OneTwoTree a)
+  deriving (Show)
 
 --Ejercicio 2
 suma :: OneTwoTree Int -> Int
-suma = undefined
+suma Void = 0
+suma (Node x t) = x + suma t
+suma (Branch x l r) = x + suma l + suma r
